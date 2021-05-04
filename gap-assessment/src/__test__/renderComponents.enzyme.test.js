f96bfdce7ce80594ff8ff2ab8c8b2aaae1ffdf76
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2021-05-04 12:08:00 -0400

    basic render test works

--- gap-assessment/src/__test__/renderComponents.enzyme.test.js
+++ gap-assessment/src/__test__/renderComponents.enzyme.test.js
@@ -2,3 +2,10 @@ import React from 'react'
 import Game from '../components/Game'
 import { shallow } from "enzyme"
 
+function renderGame() {
+	return shallow(<Game />);
+}
+
+it('Shoulder render Game',()=>{
+    const container = renderGame();
+})
