3af53393352da0fb5a6c7ffa8c22ed2808cf508b
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2021-05-03 22:02:46 -0400

    base css done, looks like the game, serves for mvp now

--- gap-assessment/src/components/Square.js
+++ gap-assessment/src/components/Square.js
@@ -3,7 +3,7 @@ import React from "react"
 
 //from game logic, ill have to pass down a value to determin is marking, and the onClick method into each sqaure
 const Square = ({value, onClick})=>{
-    const marking = value ? `squares ${value}` : `sqaures`;
+    const marking = value ? `squares ${value}` : `squares`;
 
     return(
         <button className={marking} onClick ={onClick}>
